902d1408f2406bcfaebcbddf8c63a2be88254602
Nicotcy <147240314+Nicotcy@users.noreply.github.com>
2025-12-26 15:30:41 +0100

    Update and rename middleware.ts to proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -3,12 +3,10 @@ import { createServerClient, type CookieOptions } from "@supabase/ssr";
 
 type CookieToSet = { name: string; value: string; options?: CookieOptions };
 
-export async function middleware(req: NextRequest) {
+export async function proxy(req: NextRequest) {
   const res = NextResponse.next();
 
-  // Si no tienes SUPABASE_URL/ANON en env, esto también petará en runtime,
-  // pero compilará igual.
-  createServerClient(
+  const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL as string,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
     {
@@ -24,6 +22,9 @@ export async function middleware(req: NextRequest) {
       },
     }
   );
+
+  // CLAVE: esto fuerza a Supabase a leer/refrescar sesión y escribir cookies si toca
+  await supabase.auth.getUser();
 
   return res;
 }
